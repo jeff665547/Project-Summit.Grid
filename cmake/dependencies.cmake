d848123dfcf0f75edfe2665af703c323ce9c9453
johnidfet <johnidfet@gmail.com>
2018-06-14 13:55:43 +0800

    Revert "package config"
    This reverts commit 3cf4d202cb38a674425b68943d90ac70e393b434

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,10 +1,23 @@
-# screw
-hunter_add_package(Screw)
-find_package(Screw CONFIG REQUIRED)
+# gtest
+if(BUILD_TESTS)
+    hunter_add_package(GTest)
+    find_package(GTest CONFIG REQUIRED)
+endif()
 
-# nucleona 
-hunter_add_package(NucleonaM)
-find_package(Nucleona CONFIG REQUIRED)
+# json
+hunter_add_package(nlohmann_json)
+find_package(nlohmann_json CONFIG REQUIRED)
+
+# # opencv
+# hunter_add_package(OpenCV)
+# screw_get_msvc_name(OpenCV_RUNTIME)
+# find_package(OpenCV CONFIG REQUIRED)
+# message(STATUS "OpenCV_DIR: ${OpenCV_DIR}")
+# message(STATUS "OpenCV_CONFIG: ${OpenCV_CONFIG}")
+# message(STATUS "OpenCV_LIBS: ${OpenCV_LIBS}")
+# screw_get_bits(BITS)
+# list(APPEND BUNDLE_RT_DIRS ${OpenCV_DIR}/x${BITS}/${OpenCV_RUNTIME}/bin)
+# list(APPEND BUNDLE_RT_DIRS ${OpenCV_DIR}/x${BITS}/${OpenCV_RUNTIME}/lib)
 
 # # boost 
 # hunter_add_package(Boost COMPONENTS 
@@ -20,32 +33,12 @@ find_package(Nucleona CONFIG REQUIRED)
 #     # more boost module goes here
 #     REQUIRED
 # )
-# gtest
-# if(BUILD_TESTS)
-#     hunter_add_package(GTest)
-#     find_package(GTest CONFIG REQUIRED)
-# endif()
 
-# json
-# hunter_add_package(nlohmann_json)
-# find_package(nlohmann_json CONFIG REQUIRED)
-
-
-# affy
-hunter_add_package(Affy)
-find_package(Affy CONFIG REQUIRED)
-
-# # opencv
-# hunter_add_package(OpenCV)
-# screw_get_msvc_name(OpenCV_RUNTIME)
-# find_package(OpenCV CONFIG REQUIRED)
-# message(STATUS "OpenCV_DIR: ${OpenCV_DIR}")
-# message(STATUS "OpenCV_CONFIG: ${OpenCV_CONFIG}")
-# message(STATUS "OpenCV_LIBS: ${OpenCV_LIBS}")
-# screw_get_bits(BITS)
-# list(APPEND BUNDLE_RT_DIRS ${OpenCV_DIR}/x${BITS}/${OpenCV_RUNTIME}/bin)
-# list(APPEND BUNDLE_RT_DIRS ${OpenCV_DIR}/x${BITS}/${OpenCV_RUNTIME}/lib)
+# # log4cxx
+# hunter_add_package(log4cplus)
+# find_package(log4cplus CONFIG REQUIRED)
 # 
-# # openblas
-# hunter_add_package(OpenBLAS)
-# find_package(OpenBLAS CONFIG REQUIRED)
+# # nucleona 
+# hunter_add_package(NucleonaM)
+# find_package(Nucleona CONFIG REQUIRED)
+
--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -1,43 +1,34 @@
-# if( MSVC )
-#     hunter_config(
-#         GTest
-#         VERSION ${HUNTER_GTest_VERSION}
-#         CMAKE_ARGS 
-#             CMAKE_CXX_FLAGS=/D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
-#     )
-# endif()
+if( MSVC )
+    hunter_config(
+        GTest
+        VERSION ${HUNTER_GTest_VERSION}
+        CMAKE_ARGS 
+            CMAKE_CXX_FLAGS=/D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
+    )
+else()
+    hunter_config(
+        GTest
+        VERSION ${HUNTER_GTest_VERSION}
+    )
+endif()
 hunter_config(Screw GIT_SUBMODULE "cmake/screw")
-hunter_config(NucleonaM GIT_SUBMODULE "lib/Nucleona")
-hunter_config(Affy GIT_SUBMODULE "lib/Affy"
-    CMAKE_ARGS 
-        BUILD_TESTS=OFF 
-)
-# set(str "TARGET=CORTEXA57;BINARY=64;HOSTCC=gcc")
-# hunter_config(
-#     OpenBLAS
-#     VERSION "0.2.19-p0"
+if( MINGW )
+    hunter_config(
+        OpenCV
+        VERSION ${HUNTER_OpenCV_VERSION}
+        CMAKE_ARGS 
+            BUILD_SHARED_LIBS=ON
+            ENABLE_PRECOMPILED_HEADERS=OFF
+    )
+else()
+    hunter_config(
+        OpenCV
+        VERSION ${HUNTER_OpenCV_VERSION}
+        CMAKE_ARGS BUILD_SHARED_LIBS=ON
+    )
+endif()
+
+# hunter_config(NucleonaM GIT_SUBMODULE "lib/Nucleona"
 #     CMAKE_ARGS 
-#         target_arg=${str}
-# )
-# if( MINGW )
-#     hunter_config(
-#         OpenCV
-#         VERSION ${HUNTER_OpenCV_VERSION}
-#         CMAKE_ARGS 
-# 	    BUILD_SHARED_LIBS=OFF
-#             ENABLE_PRECOMPILED_HEADERS=OFF
-# 	    WITH_PROTOBUF=OFF
-#     )
-# else()
-#     hunter_config(
-#         OpenCV
-#         VERSION ${HUNTER_OpenCV_VERSION}
-# 	    CMAKE_ARGS 
-#             BUILD_SHARED_LIBS=OFF
-#             WITH_PROTOBUF=OFF
-#     )
-# endif()
-# hunter_config(
-#     Boost
-#     VERSION "1.64.0"
+#         BUILD_TESTS=OFF 
 # )
