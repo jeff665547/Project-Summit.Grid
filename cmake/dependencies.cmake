aa5c54208fd9326b2239507cd842feccc8de965d
ChiaHuaChang <johnidfet@gmail.com>
2018-08-08 14:31:29 +0800

    upstream package done

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,23 +1,16 @@
 # gtest
-# if(BUILD_TESTS)
-#     hunter_add_package(GTest)
-#     find_package(GTest CONFIG REQUIRED)
-# endif()
+if(BUILD_TESTS)
+    hunter_add_package(GTest)
+    find_package(GTest CONFIG REQUIRED)
+endif()
 
 # json
-# hunter_add_package(nlohmann_json)
-# find_package(nlohmann_json CONFIG REQUIRED)
+hunter_add_package(nlohmann_json)
+find_package(nlohmann_json CONFIG REQUIRED)
 
-# # opencv
-# hunter_add_package(OpenCV)
-# screw_get_msvc_name(OpenCV_RUNTIME)
-# find_package(OpenCV CONFIG REQUIRED)
-# message(STATUS "OpenCV_DIR: ${OpenCV_DIR}")
-# message(STATUS "OpenCV_CONFIG: ${OpenCV_CONFIG}")
-# message(STATUS "OpenCV_LIBS: ${OpenCV_LIBS}")
-# screw_get_bits(BITS)
-# list(APPEND BUNDLE_RT_DIRS ${OpenCV_DIR}/x${BITS}/${OpenCV_RUNTIME}/bin)
-# list(APPEND BUNDLE_RT_DIRS ${OpenCV_DIR}/x${BITS}/${OpenCV_RUNTIME}/lib)
+# nucleona 
+hunter_add_package(NucleonaM)
+find_package(Nucleona CONFIG REQUIRED)
 
 # # boost 
 # hunter_add_package(Boost COMPONENTS 
@@ -34,12 +27,8 @@
 #     REQUIRED
 # )
 
-# # log4cxx
-# hunter_add_package(log4cplus)
-# find_package(log4cplus CONFIG REQUIRED)
-# 
-# # nucleona 
-# hunter_add_package(NucleonaM)
-# find_package(Nucleona CONFIG REQUIRED)
+# ChipImgProc 
+hunter_add_package(ChipImgProc)
+find_package(ChipImgProc CONFIG REQUIRED)
 
 include(${SCREW_DIR}/hunter_root.cmake)
--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -1,34 +1,39 @@
 hunter_config(Screw GIT_SUBMODULE "cmake/screw")
-# if( MSVC )
-#     hunter_config(
-#         GTest
-#         VERSION ${HUNTER_GTest_VERSION}
-#         CMAKE_ARGS 
-#             CMAKE_CXX_FLAGS=/D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
-#     )
-# else()
-#     hunter_config(
-#         GTest
-#         VERSION ${HUNTER_GTest_VERSION}
-#     )
-# endif()
-# if( MINGW )
-#     hunter_config(
-#         OpenCV
-#         VERSION ${HUNTER_OpenCV_VERSION}
-#         CMAKE_ARGS 
-#             BUILD_SHARED_LIBS=ON
-#             ENABLE_PRECOMPILED_HEADERS=OFF
-#     )
-# else()
-#     hunter_config(
-#         OpenCV
-#         VERSION ${HUNTER_OpenCV_VERSION}
-#         CMAKE_ARGS BUILD_SHARED_LIBS=ON
-#     )
-# endif()
+hunter_config(ChipImgProc GIT_SUBMODULE "lib/ChipImgProc"
+    CMAKE_ARGS
+        BUILD_TESTS=OFF
+)
+hunter_config(NucleonaM GIT_SUBMODULE "lib/Nucleona"
+    CMAKE_ARGS 
+        BUILD_TESTS=OFF 
+        ENABLE_HDF5=ON
+)
 
-# hunter_config(NucleonaM GIT_SUBMODULE "lib/Nucleona"
-#     CMAKE_ARGS 
-#         BUILD_TESTS=OFF 
-# )
+if( MSVC )
+    hunter_config(
+        GTest
+        VERSION ${HUNTER_GTest_VERSION}
+        CMAKE_ARGS 
+            CMAKE_CXX_FLAGS=/D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
+    )
+else()
+    hunter_config(
+        GTest
+        VERSION ${HUNTER_GTest_VERSION}
+    )
+endif()
+if( MINGW )
+    hunter_config(
+        OpenCV
+        VERSION "3.4.0-p0"
+        CMAKE_ARGS 
+            BUILD_SHARED_LIBS=ON
+            ENABLE_PRECOMPILED_HEADERS=OFF
+    )
+else()
+    hunter_config(
+        OpenCV
+        VERSION "3.4.0-p0"
+        CMAKE_ARGS BUILD_SHARED_LIBS=ON
+    )
+endif()
