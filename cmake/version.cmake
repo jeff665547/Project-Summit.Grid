fd6c2693b1e8987e92ab0d19492fcd179eb64647
John Chang <CHChang810716@gmail.com>
2018-09-28 02:33:00 +0800

    add complete file, update version (0.0.5)

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,2 +1,2 @@
-screw_add_version( 0 0 4 "${CMAKE_SOURCE_DIR}/include/summit/version.h")
+screw_add_version( 0 0 5 "${CMAKE_SOURCE_DIR}/include/summit/version.h")
 message(STATUS "${PROJECT_NAME} v${${PROJECT_NAME}_VERSION}")
