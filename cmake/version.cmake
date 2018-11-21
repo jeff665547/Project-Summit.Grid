11d161388b4cac0fdc35abd91a7b8d7bc2dd4c0d
Chia Hua Chang <johnidfet@gmail.com>
2018-11-21 19:15:42 +0800

    update version

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,2 +1,2 @@
-screw_add_version( 0 0 5 "${CMAKE_SOURCE_DIR}/include/summit/version.h")
+screw_add_version( 0 0 6 "${CMAKE_SOURCE_DIR}/include/summit/version.h")
 message(STATUS "${PROJECT_NAME} v${${PROJECT_NAME}_VERSION}")
