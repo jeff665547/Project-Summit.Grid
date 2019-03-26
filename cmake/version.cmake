31f4f9481c6880de64f2cbc46c0dce96727494ba
Chia Hua Chang <johnidfet@gmail.com>
2019-03-26 11:49:31 +0800

    update version 0.8.3

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,2 +1,2 @@
-screw_add_version( 0 8 2 "${CMAKE_SOURCE_DIR}/include/summit/version.h")
+screw_add_version( 0 8 3 "${CMAKE_SOURCE_DIR}/include/summit/version.h")
 message(STATUS "${PROJECT_NAME} v${${PROJECT_NAME}_VERSION}")
