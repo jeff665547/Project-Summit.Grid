408a6f33c048b55b3c63e146fccb34f0aceec32c
Chia Hua Chang <johnidfet@gmail.com>
2019-04-11 11:09:11 +0800

    update light_mean run script

--- src/script/light_mean_banff_CY3.bat
+++ src/script/light_mean_banff_CY3.bat
@@ -1,4 +1,4 @@
 SET ROOT=%~dp0
-%root%\..\bin\summit-app-light_mean.exe %* 980 light_mean_CY3.tsv
+%root%\..\bin\summit-app-light_mean.exe -i %* -o light_mean_CY3.tsv -c banff -m AM1
 
 pause
--- src/script/light_mean_banff_CY5.bat
+++ src/script/light_mean_banff_CY5.bat
@@ -1,4 +1,4 @@
 SET ROOT=%~dp0
-%root%\..\bin\summit-app-light_mean.exe %* 392 light_mean_CY5.tsv
+%root%\..\bin\summit-app-light_mean.exe -i %* -o light_mean_CY5.tsv -c banff -m AM3
 
 pause
