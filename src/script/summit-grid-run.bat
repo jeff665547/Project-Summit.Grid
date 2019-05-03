1bc28c287fea0c7413ccc9c1686dc77349bb5e0b
Chia Hua Chang <johnidfet@gmail.com>
2019-05-03 10:27:39 +0800

    update: run script add marker append, threading, close debug mode

--- src/script/summit-grid-run.bat
+++ src/script/summit-grid-run.bat
@@ -1,4 +1,4 @@
 SET ROOT=%~dp0
-%root%\..\bin\summit-app-grid.exe -o output -i %* -r "tsv,html" -d 1
+%root%\..\bin\summit-app-grid.exe -o output -i %* -r "tsv,html" -d 0 --marker_append --no_bgp -n 2
 
 pause
