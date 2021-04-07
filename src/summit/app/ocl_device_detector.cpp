7864c0f62db770864457771a3658c37c03617015
noreason <ken5112840801@gmail.com>
2021-04-07 18:45:55 +0800

    [Feature] Only read system environment variable in ocl_device_detector.hpp

--- src/summit/app/ocl_device_detector.cpp
+++ src/summit/app/ocl_device_detector.cpp
@@ -1,27 +1,29 @@
 #include <opencv2/core/ocl.hpp>
 
+#include <cstdlib>
 #include <iostream>
-#include <stdlib.h>
+#include <string>
 
 int main()
 {
-    char* libvar;
-    _putenv_s("OPENCV_OPENCL_DEVICE", "");
-    const char* env = getenv("OPENCV_OPENCL_DEVICE");
-    if (env)
-        std::cout << "OPENCV_OPENCL_DEVICE: " << *env << std::endl;
-
+    const char* c_var(std::getenv("OPENCV_OPENCL_DEVICE"));
+    std::string var;
+    if (c_var != nullptr)
+        var = c_var;
+    std::cout << "OPENCV_OPENCL_DEVICE=" << c_var << std::endl;
+    
     cv::ocl::Context context;
     if (!context.create(cv::ocl::Device::TYPE_ALL))
         std::cout << "OpenCL not available...\n";
-
-    std::cout << context.ndevices() << " devices are detected:\n";
-    for (int i = 0; i < context.ndevices(); i++) {
-        auto&& device = context.device(0);
-        std::cout << "name:              " << device.name() << std::endl;
-        std::cout << "available:         " << device.available() << std::endl;
-        std::cout << "imageSupport:      " << device.imageSupport() << std::endl;
-        std::cout << "OpenCL_C_Version:  " << device.OpenCL_C_Version() << std::endl;
-        std::cout << std::endl;
+    else {
+        std::cout << context.ndevices() << " devices are detected:\n";
+        for (int i = 0; i < context.ndevices(); i++) {
+            auto&& device = context.device(i);
+            std::cout << "name:              " << device.name() << std::endl;
+            std::cout << "available:         " << device.available() << std::endl;
+            std::cout << "imageSupport:      " << device.imageSupport() << std::endl;
+            std::cout << "OpenCL_C_Version:  " << device.OpenCL_C_Version() << std::endl;
+            std::cout << std::endl;
+        }
     }
 }
